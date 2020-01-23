b6119a2145096df6b0179276665664edf9555fb3
Paul Lorenz <paul.lorenz@netfoundry.io>
2020-01-23 15:53:11 -0500

    Remove unused code

--- tunnel/entities/service.go
+++ tunnel/entities/service.go
@@ -1,6 +1,24 @@
 package entities
 
-import "github.com/netfoundry/ziti-sdk-golang/ziti/edge"
+import (
+	"fmt"
+	"github.com/netfoundry/ziti-sdk-golang/ziti/edge"
+)
+
+const (
+	ClientConfigV1 = "ziti-tunneler-client.v1"
+	ServerConfigV1 = "ziti-tunneler-server.v1"
+)
+
+type ServiceConfig struct {
+	Protocol string
+	Hostname string
+	Port     int
+}
+
+func (s *ServiceConfig) String() string {
+	return fmt.Sprintf("%v:%v:%v", s.Protocol, s.Hostname, s.Port)
+}
 
 type Service struct {
 	edge.Service
