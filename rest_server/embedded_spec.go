40bdbcf031a8b9bdbd22717ebc90f4c4570b188f
Andrew Martinez <andrew.p.martinez@gmail.com>
2020-12-18 11:05:20 -0500

    run generation on new spagger.yml

--- rest_server/embedded_spec.go
+++ rest_server/embedded_spec.go
@@ -1154,7 +1154,7 @@ func init() {
         ],
         "description": "Terminates the current API session",
         "tags": [
-          "CurrentAPI Session"
+          "Current API Session"
         ],
         "summary": "Logout",
         "responses": {
@@ -13019,7 +13019,7 @@ func init() {
         ],
         "description": "Terminates the current API session",
         "tags": [
-          "CurrentAPI Session"
+          "Current API Session"
         ],
         "summary": "Logout",
         "responses": {
